8efcb0160f86cf4b42a4aa6cc797c3331f1ad07d
hp-cx0003nm <ivan.jovanovitsh.mb@gmail.com>
2020-05-16 18:41:53 +0200

    Dodano Osnova angular in express aplikacije za začetek razvoja

--- back-end/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('To je naš uporabnik');
-});
-
-module.exports = router;
