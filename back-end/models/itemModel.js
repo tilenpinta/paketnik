de787ef7f6a4017520d6998b70da06005733b794
pinta <tilen.pintaric@student.um.si>
2020-05-20 19:23:45 +0200

    fixing barcode

--- back-end/models/itemModel.js
+++ back-end/models/itemModel.js
@@ -22,18 +22,47 @@ itemSchema.statics.generateBarcode = () => {
 	let barcode = '';
 	for (let i = 0; i < 12; i++ ) {
 		barcode += (Math.floor(Math.random() * 10)).toString();
+		console.log(barcode);
 	}
 
+	console.log("\n");
+
+	let sumEven = 0;
+	let sumOdd = 0;
+	let num = 0;
+
+	for(let i = 0; i < 11; i++) {
+		num = parseInt(barcode);
+	}
+	console.log(num);
+}
+
+/*
+itemSchema.statics.generateBarcode = () => {
+	let barcode = '';
+	for (let i = 0; i < 13; i++ ) {
+		barcode += (Math.floor(Math.random() * 10)).toString();
+		console.log("barcode");
+		console.log(barcode);
+		console.log();
+	}
+
+
 	gtin = parseInt( barcode, 10 ).toString();
+	console.log(gtin);
 	let chunks = gtin.split('').map( toInt ).reverse();
+	console.log("\n" + chunks);
 	let checksum = 0;
 
 	// Remove first chuck (checksum)
 	chunks.shift();
 
+	console.log("\n" + chunks + "\n");
+
 	// sum numbers and multiply accordingly
 	chunks.forEach( (number, index) => {
 		checksum += isOdd(index) ? number : number*3;
+		console.log(checksum);
 	});
 	// calc checksum
 	checksum %= 10;
@@ -41,6 +70,7 @@ itemSchema.statics.generateBarcode = () => {
 	const newBarcode = barcode + checksum.toString();
 	return newBarcode;
 }
+*/
 
 const Item = mongoose.model('Item', itemSchema);
 
